9c6d97639ab77503fc64a818e59f9a644de2a5ae
hyeyoooo <155596443+hyeyoooo@users.noreply.github.com>
2024-06-13 15:51:01 +0900

    [Gold IV] Title: PPAP, Time: 36 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ using namespace std;
+
+int main(void){
+	string s;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < s.length(); i++){
+      	if (s[i] == 'P'){
+            cnt++;
+            continue;
+        }
+        if (cnt >= 2 && s[i + 1] == 'P'){
+            cnt--;
+            i++;
+        }else{
+            cout << "NP\n";
+            return 0;
+        }
+    }
+    
+    if (cnt == 1){
+        cout << "PPAP\n";
+    }else{
+        cout << "NP\n";
+    }
+    return 0;
+}
